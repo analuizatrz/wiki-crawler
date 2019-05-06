18801868619ccbf9fa778a989ed2b62d59825827
Ana Luiza Sanches <analuizatrz@gmail.com>
2019-05-05 23:36:15 -0300

    refactor main

--- src/main.py
+++ src/main.py
@@ -3,21 +3,14 @@ from wiki_crawler import WikiPagesCrawler
 from wiki_parser import WikiParserPageWithClass, WikiParserCategory
 import sys
 
-def wiki_page_class_crawler(input_file_name):
+def wiki_crawl(input_file, parser):
 	input_file_name = file_name_with_out_extension(input_file)
 	articles = read_first_column(input_file)
 
 	WikiPagesCrawler(input_file_name, "2017-10-01T00:00:00Z", "desc", 1, 3)\
-		.request_in_bulks(articles, WikiParserPageWithClass())
-
-def wiki_pages_from_category(input_file):
-	input_file_name = file_name_with_out_extension(input_file)
-	articles = read_first_column(input_file)
-
-	WikiPagesCrawler(input_file_name, "2017-10-01T00:00:00Z", "desc", 1, 3)\
-		.request_in_bulks(articles, WikiParserPageWithClass())
+		.request_in_bulks(articles, parser)
 
 if __name__ == "__main__":
 	input_file = sys.argv[1]
-	wiki_page_class_crawler(input_file)
-	wiki_pages_from_category(input_file)
+	wiki_crawl(input_file, WikiParserPageWithClass())
+	#wiki_crawl(input_file, WikiParserCategory())
