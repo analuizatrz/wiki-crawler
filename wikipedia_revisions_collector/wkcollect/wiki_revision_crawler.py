e64b7378c1729fcb720d13260ab654c5fd196b43
Ana Luiza Sanches <analuizatrz@gmail.com>
2019-10-05 14:31:30 -0300

    begin refactoring: extracting request functions

--- wikipedia_revisions_collector/wkcollect/wiki_revision_crawler.py
+++ wikipedia_revisions_collector/wkcollect/wiki_revision_crawler.py
@@ -3,9 +3,9 @@ import json
 import time
 from pandas.io.json import json_normalize
 from datetime import datetime
-import requests
 import os
 import csv
+from wkrequest import get_revisions_info, get_titles_from_id, get_revision_content
 
 file_log = "log.csv"
 
@@ -102,70 +102,6 @@ def parse_revision_content(response):
     revision = list(page["revisions"])[0]
    # return (page["pageid"], page["title"], revision["user"], revision["timestamp"], revision["comment"], revision["slots"]["main"]["content"])
     return revision["slots"]["main"]["content"]
-
-
-S = requests.Session()
-URL = "https://en.wikipedia.org/w/api.php"
-
-
-def get_revisions_info(title, date_start, date_end, rvcontinue=None):
-    """Resquest revisions info of a wikipedia article by its title, in a period determined by start and end dates
-
-    Parameters:
-        title (str): The title of the article
-        date_start (str): The start date of querying in the format ISO 8601: 2001-01-15T14:56:00Z
-        date_end (str): The end date of querying in the format ISO 8601: 2001-01-15T14:56:00Z
-
-    Returns:
-        response (json): raw result of the request
-    """
-    params = {
-        "action": "query",
-        "prop": "revisions",
-        "format": "json",
-        "titles": title,
-        "rvlimit": 500,
-        "rvprop": "ids|timestamp|user|comment",
-        "rvstart": date_start,
-        "rvend": date_end,
-        "rvdir": "older",
-        "redirects": 1
-    }
-    if rvcontinue is not None:
-        params["rvcontinue"] = rvcontinue
-
-    response = S.get(url=URL, params=params)
-
-    return response.json()
-
-def get_revision_content(title, access_date):
-    params = {
-        "action": "query",
-        "prop": "revisions",
-        "titles": title,
-        "rvprop": "timestamp|user|comment|content|ids",
-        "rvslots": "main",
-        "formatversion": "2",
-        "format": "json",
-        "rvlimit": 1,
-        "rvstart": access_date,
-        "rvdir": "older",
-    }
-
-    response = S.get(url=URL, params=params)
-    
-    return response.json()
-
-def get_titles_from_id(ids):
-    params = {
-        "action": "query",
-        "format": "json",
-        "pageids": "|".join(ids)
-    }
-
-    response = S.get(url=URL, params=params)
-    
-    return response.json()
 
 def read_json(file_name):
     with open(file_name, 'r') as fp:
@@ -319,8 +255,6 @@ def run_collect_all_content_2009_2007(base_folder):
     collect_all(titles, collect_content, params, output_folder)
 
 def collect_all_titles(base_folder):
-    params = Params()
-
     output_folder = f"{base_folder}/data/titles"
     input_file = f"{base_folder}/ids.csv"
     create_folder_if_does_not_exist(output_folder)
@@ -339,8 +273,11 @@ def run_test(base_folder):
     collect_revisions_info(title, params, f"{base_folder}/test/"), 
 
 if __name__ == "__main__":
-    base_folder = "/home/ana/Documents/tcc-web-crawler"
-    collect_all_titles(base_folder)
+    get_titles_from_id_result = get_titles_from_id(ids=["45492", "58791", "171020"])
+    print(f"get_titles_from_id: \n{get_titles_from_id_result}")
+
+    # base_folder = "/home/ana/Documents/tcc-web-crawler"
+    # collect_all_titles(base_folder)
     # run_collect_all_revision_info_2009_2007(base_folder)
     # run_collect_all_content_2009_2007(base_folder)
 
