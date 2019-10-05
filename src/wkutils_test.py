bacc0af631c628ddf2bef684afb1ccfe38f10bc0
Ana Luiza Sanches <analuizatrz@gmail.com>
2019-10-05 19:01:34 -0300

    fix utils test

--- src/wkutils_test.py
+++ src/wkutils_test.py
@@ -6,7 +6,8 @@ class TestWikiParser(unittest.TestCase):
 
     def call_match_dates_and_revisions(self, dates, revisions):
         r = [{"timestamp": x} for x in revisions]
-        return match_dates_and_revisions(dates, r)
+        result, is_complete, a = match_dates_and_revisions(dates, r)
+        return result
 
     def test_match_dates_revisions(self):
         """
@@ -19,12 +20,10 @@ class TestWikiParser(unittest.TestCase):
         self.assertEqual(actual[0]["access"], 9)
         self.assertEqual(actual[1]["access"], 6)
         self.assertEqual(actual[2]["access"], 3)
-        self.assertEqual(actual[3]["access"], 0)
 
         self.assertEqual(actual[0]["revision"]["timestamp"], 9)
         self.assertEqual(actual[1]["revision"]["timestamp"], 6)
         self.assertEqual(actual[2]["revision"]["timestamp"], 2)
-        self.assertEqual(actual[3]["revision"], None)
 
         dates = [8, 5, 2, 1]
         revisions = [9, 6, 5, 4, 2, 1]
@@ -51,19 +50,6 @@ class TestWikiParser(unittest.TestCase):
         self.assertEqual(actual[0]["revision"]["timestamp"], 6)
         self.assertEqual(actual[1]["revision"]["timestamp"], 6)
         self.assertEqual(actual[2]["revision"]["timestamp"], 6)
-
-        dates = [9, 8, 7]
-        revisions = [11, 10]
-        actual = self.call_match_dates_and_revisions(dates, revisions)
-
-        self.assertEqual(actual[0]["access"], 9)
-        self.assertEqual(actual[1]["access"], 8)
-        self.assertEqual(actual[2]["access"], 7)
-
-        self.assertEqual(actual[0]["revision"], None)
-        self.assertEqual(actual[1]["revision"], None)
-        self.assertEqual(actual[2]["revision"], None)
-
 
 if __name__ == '__main__':
     unittest.main()
