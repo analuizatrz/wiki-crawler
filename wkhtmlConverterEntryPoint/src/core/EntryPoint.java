b3ea0cab6dfd578f0d9cc16677fe2c98d66d2726
Ana Luiza Sanches <analuizatrz@gmail.com>
2019-10-09 10:36:02 -0300

    criate an executable .jar for HtmlConverterEntryPoint project and add a readme

--- wkhtmlConverterEntryPoint/src/core/EntryPoint.java
+++ wkhtmlConverterEntryPoint/src/core/EntryPoint.java
@@ -4,6 +4,13 @@ import py4j.GatewayServer;
 
 public class EntryPoint {
 
+	public static void main(String[] args) {
+		System.out.println("oi");
+	    GatewayServer gatewayServer = new GatewayServer(new EntryPoint());
+	    gatewayServer.start();
+	    System.out.println("Gateway Server Started : WikiHtmlConverter");		
+
+	}
 	EntryPoint(){
 		converter = new Converter();	
 	}
@@ -13,11 +20,6 @@ public class EntryPoint {
 	public Converter getConverter() {
         return converter;
     }
-	public static void main(String[] args) {
-		System.out.println("oi");
-	    GatewayServer gatewayServer = new GatewayServer(new EntryPoint());
-	    gatewayServer.start();
-	    System.out.println("Gateway Server Started : WikiHtmlConverter");		
 
-	}
+
 }
