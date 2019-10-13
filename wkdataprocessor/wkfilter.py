fa8e69dc68a400d3d9783c2b3edaeac6fd39e523
Ana Luiza Sanches <analuizatrz@gmail.com>
2019-10-13 19:12:49 -0300

    separate invalid row filtered by category

---
+++ wkdataprocessor/wkfilter.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+def where_not_in(df, column, values):
+    for value in values:
+        df = df[df[column] != value]
+    return df
+
+def split_valid_invalid(df):
+    valid_categories = ['A','B','C','FA','GA','Start','Stub']
+
+    invalid = where_not_in(df, 'category', valid_categories)
+    valid = df.drop(invalid.index)
+
+    return valid, invalid
+
+if __name__ == "__main__":
+
+    base_folder = "/home/ana/Documents/tcc-web-crawler"
+    joined_df = pd.read_csv(f"{base_folder}/join_features.csv")
+    
+    valid, invalid = split_valid_invalid(joined_df)
+
+    invalid.to_csv(f"{base_folder}/join_features-invalid-categories.csv")
+    valid.to_csv(f"{base_folder}/join_features-valid-categories.csv")
--- wkdataprocessor/wkjoin.py
+++ wkdataprocessor/wkjoin.py
@@ -2,8 +2,9 @@ import os
 import csv
 import pandas as pd
 
+
 def join_all(datasets_folder):
-    li=[]
+    li = []
     titles = os.listdir(datasets_folder)
     for index, title in enumerate(titles):
         df = pd.read_csv(f"{datasets_folder}/{title}")
@@ -12,19 +13,23 @@ def join_all(datasets_folder):
 
     return pd.concat(li, axis=0, ignore_index=True)
 
+
 def df_first_columns(df, columns):
-    cols = columns + [col for col in df if col not in columns]
-    return df[cols]
+    new_columns_order = columns + [col for col in df if col not in columns]
+    return df[new_columns_order]
 
-base_folder = "/home/ana/Documents/tcc-collected-data/data"
-category_folder = f"{base_folder}/info_and_category_200701-200901/data" 
 
-features = pd.read_csv(f"{base_folder}/features/data/raw_dataset.csv", sep=";")
-category = join_all(category_folder)
-category.columns = ['access', 'category', 'pageid', 'raw_category', 'revision.anon',
-       'revision.comment', 'revision.commenthidden', 'revision.parentid',
-       'revision.revid', 'timestamp', 'revision.user', 'title']
+if __name__ == "__main__":
 
-merged = pd.merge(category, features, how='inner').sort_values(by=['title','timestamp'], ascending=[True, False])
+    base_folder = "/home/ana/Documents/tcc-collected-data/data"
+    category_folder = f"{base_folder}/info_and_category_200701-200901/data"
 
-df_first_columns(merged,['title','timestamp']).to_csv(f"features.csv", index=None, header=True, quoting=csv.QUOTE_NONNUMERIC)
+    features = pd.read_csv(f"{base_folder}/features/data/raw_dataset.csv", sep=";")
+    category = join_all(category_folder)
+    category.columns = ['access', 'category', 'pageid', 'raw_category', 'revision.anon',
+                        'revision.comment', 'revision.commenthidden', 'revision.parentid',
+                        'revision.revid', 'timestamp', 'revision.user', 'title']
+if __name__ == "__main__":
+    merged = pd.merge(category, features, how='inner').sort_values(by=['title', 'timestamp'], ascending=[True, False])
+
+    df_first_columns(merged, ['title', 'timestamp']).to_csv(f"features.csv", index=None, header=True, quoting=csv.QUOTE_NONNUMERIC)
